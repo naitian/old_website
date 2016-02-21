02788555733d2c4d863f8b34047309b10059b88b
Naitian Zhou <nzhou2011@gmail.com>
2016-02-21 16:37:40 -0500

    oops es6 ain't everywhere

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-$("document").ready(function(){
-	$("#input").keypress((e) => {
-		if(e.which == 13){
+"use strict";
+$("document").ready(function () {
+	$("#input").keypress(function (e) {
+		if (e.which == 13) {
 			e.preventDefault();
 			input($("#input").text());
 		}
 	});
 
-	const allPossible = [
-		"about",
-		"open",
-		"ls",
-		"projects",
-		"contact"
-	];
+	var allPossible = ["about", "open", "ls", "projects", "contact"];
 
-	$("#input").keydown((e) => {
+	$("#input").keydown(function (e) {
 		var node = window.getSelection().anchorNode;
 		var suggestions = [];
 		for (var i = allPossible.length - 1; i >= 0; i--) {
-			if(allPossible[i].indexOf(node.data.substring(node.data.lastIndexOf(" ") + 1)) === 0){
+			if (allPossible[i].indexOf(node.data.substring(node.data.lastIndexOf(" ") + 1)) === 0) {
 				suggestions.push(allPossible[i]);
 			}
 		}
-		if(e.which == 9){
+		if (e.which == 9) {
 			e.preventDefault();
-			if(suggestions.length == 1){
+			if (suggestions.length == 1) {
 				var sel = window.getSelection();
 				var range = sel.getRangeAt(0);
 				node.data = (node.data.substring(0, node.data.lastIndexOf(" ")) + " " + suggestions[0]).trim();
@@ -35,65 +30,58 @@ $("document").ready(function(){
 		}
 	});
 
-
 	var today = new Date();
-	if(today.getHours() > 20 || today.getHours() < 3)
-		$("#about").addClass("night");
+	if (today.getHours() > 20 || today.getHours() < 3) $("#about").addClass("night");
 
-	if($("#overlay").css("pointer-events") == "none"){
+	if ($("#overlay").css("pointer-events") == "none") {
 		$("#input").attr("contenteditable", "false");
 	}
 
-	$("#overlay").click(function(){
-		if($("#overlay").css("pointer-events") != "none"){
+	$("#overlay").click(function () {
+		if ($("#overlay").css("pointer-events") != "none") {
 			$("#input").attr("contenteditable", "true");
-		}
-		else {
+		} else {
 			$("#input").attr("contenteditable", "false");
-
 		}
 		$("#input").focus();
 	});
 	$("#input").focus();
 	// $(".project-frame").slick({
-	// 	dots: true, 
+	// 	dots: true,
 	// })
 
-	$(".toggle-night").click(() => {
+	$(".toggle-night").click(function () {
 		$("#about").toggleClass("night");
 	});
 });
 
-function input(input){
+function input(input) {
 	if (input.length === 0) {
 		return;
 	}
 
 	var parsed = parse(input);
 	var out = "";
-	if(parsed.cmd == "open"){
+	if (parsed.cmd == "open") {
 		var target = "#" + parsed.arg;
 		$.smoothScroll({
 			scrollTarget: target
-	    });
-	}
-	else if(parsed.cmd == "ls"){
+		});
+	} else if (parsed.cmd == "ls") {
 		out = "about&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;projects&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;contact";
-	}
-	else {
+	} else {
 		out = "That command is not yet supported. Try ls or open.";
 	}
 
 	$("#content").append("<br>$ " + input);
 	$("#input").empty();
-	if(out.length > 0)
-		$("#content").append("<br>" + out);
+	if (out.length > 0) $("#content").append("<br>" + out);
 }
 
-function parse(cmd){
+function parse(cmd) {
 	var cmdAr = cmd.split(" ");
 	var command = cmdAr[0];
 	var arg = cmdAr[cmdAr.length - 1];
 
-	return {"cmd": command, "arg": arg};
+	return { "cmd": command, "arg": arg };
 }
